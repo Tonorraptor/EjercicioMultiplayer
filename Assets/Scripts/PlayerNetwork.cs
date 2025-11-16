2af3ba1dfa92e3328ed5ec4ebfe0f188ef29ab96
Julio Celestino <velocirraptorrex3@gmail.com>
2025-11-16 16:23:10 -0500

    Add Color player

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,21 +1,29 @@
-using UnityEngine;
-using Unity.Netcode;
+using System;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Collections;
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
     private NetworkVariable<FixedString32Bytes> playerName = new NetworkVariable<FixedString32Bytes>(string.Empty);
+    //private NetworkVariable<int> materialIndex = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    private NetworkVariable<int> materialIndex = new NetworkVariable<int>(0);
+
 
     private Rigidbody rb;
     [SerializeField] private float speed;
-    [SerializeField] private List<Material> materialsPrefab = new List<Material>();
+    [SerializeField] private List<Material> materialPrefab;
     [SerializeField] private TextMeshPro nameText;
+
+    private Renderer materialRenderer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        materialRenderer = GetComponent<Renderer>();
     }
 
     public override void OnNetworkSpawn()
@@ -24,6 +32,12 @@ public class PlayerNetwork : NetworkBehaviour
         playerName.OnValueChanged += (oldValue, newValue) =>
         {
             nameText.text = newValue.ToString();
+        };
+
+        ApplyMaterial(materialIndex.Value);
+        materialIndex.OnValueChanged += (oldValue, newValue) =>
+        {
+            ApplyMaterial(newValue);
         };
     }
     public void SetName(string name)
@@ -41,16 +55,43 @@ public class PlayerNetwork : NetworkBehaviour
         SendNameToClientRpc(name);
     }
 
-    [Rpc(SendTo.Server)]
+    [Rpc(SendTo.ClientsAndHost)]
     private void SendNameToClientRpc(string name)
     {
         nameText.text = name;
     }
-    private void Start()
+
+    public void AssignMaterial(int index)
+    {
+        /*int randomIndex = Random.Range(0, materialPrefab.Count);
+        materialIndex.Value = randomIndex;*/
+
+        if (IsOwner)
+        {
+            SendMaterialToServerRpc(index);
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SendMaterialToServerRpc(int index)
+    {
+        if(index >= 0 && index < materialPrefab.Count)
+        {
+            materialIndex.Value = index;
+        }
+    }
+    private void ApplyMaterial(int index)
+    {
+        if (materialPrefab.Count == 0) return;
+        materialRenderer.material = materialPrefab[index];
+    }
+
+    /*private void Start()
     {
         if (IsOwner) GetComponent<Renderer>().material = materialsPrefab[0];
         else GetComponent<Renderer>().material = materialsPrefab[1];
-    }
+    }*/
+
     private void Update()
     {
         if (IsOwner)
--- Assets/Scripts/UIScripting.cs
+++ Assets/Scripts/UIScripting.cs
@@ -5,18 +5,22 @@ using UnityEngine.UI;
 public class UIScripting : MonoBehaviour
 {
     [SerializeField] private TMP_InputField nameInputField;
+    [SerializeField] private TMP_Dropdown dropdown;
     [SerializeField] private Button Button;
     [SerializeField] private GameObject panel;
     private void Awake()
     {
-        Button.onClick.AddListener(SetName);
+        Button.onClick.AddListener(SetConfiguration);
     }
-    private void SetName()
+    private void SetConfiguration()
     {
         GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
         foreach(GameObject p in player)
         {
-            p.GetComponent<PlayerNetwork>().SetName(nameInputField.text);
+            var configuration = p.GetComponent<PlayerNetwork>();
+
+            configuration.SetName(nameInputField.text);
+            configuration.AssignMaterial(dropdown.value);
         }
         Destroy(panel);
      }
